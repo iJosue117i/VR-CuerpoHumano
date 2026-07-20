cac0d2ce583e839bb5ec44f28097b82a98d11575
JosueACR <josuecastillo117@gmail.com>
2026-07-19 20:51:45 -0500

    v0.02

---
+++ Assets/Resources/Scripts/HandPresence.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public XRNode inputSource;
+    public Animator handAnim;
+
+    public float gripValue;
+    public float triggerValue;
+
+    
+    // Start is called before the first frame update
+    enum HandGrab { none = 0, ball = 1, gun = 2 };
+    void UpdateHandAnimation(InputDevice device)
+    {       
+
+        if (device.TryGetFeatureValue(CommonUsages.trigger, out triggerValue))
+        {
+            handAnim.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnim.SetFloat("Trigger", 0);
+        }
+        if (device.TryGetFeatureValue(CommonUsages.grip, out gripValue))
+        {
+            handAnim.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnim.SetFloat("Grip", 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+
+        UpdateHandAnimation(device);
+    }
+
+    
+
+}
---
+++ Assets/Resources/Scripts/S_ControlEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class S_ControlEffect : MonoBehaviour
+{
+    public XRController control;    
+
+    public void Vibracion()
+    {
+        StartCoroutine(vibrar());
+    }
+    IEnumerator vibrar()
+    {
+        yield return new WaitForSeconds(0.1f);
+        control.SendHapticImpulse(0.9f, 1.5f);
+
+    }
+    public void VibracionPersonalizada(float fuerza)
+    {
+        StartCoroutine("vibrarPersonalizado",fuerza);
+    }
+    IEnumerator vibrarPersonalizado(float fuerza)
+    {
+        yield return new WaitForSeconds(0.1f);
+        control.SendHapticImpulse(fuerza, 0.5f);
+
+    }
+}
