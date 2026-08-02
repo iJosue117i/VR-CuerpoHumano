745dfd05db9c70ec7aef085c2f6280f2c16d2ccc
JosueACR <josuecastillo117@gmail.com>
2026-08-02 17:59:30 -0500

    v0.12

--- Assets/Resources/Scripts/S_Enzima.cs
+++ Assets/Resources/Scripts/S_Enzima.cs
@@ -1,14 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class S_Enzima : MonoBehaviour
 {
     public Rigidbody rb;
     public float time = 2;
+    float velocidad = 1.25f;
+    public float finalPosY;
+
+    public GameObject efectoPepsina;
+    public Transform particulaHija;
+    public Vector3 velocidadRotacion;
+    public bool activateRotation = true;
+    public float limitMax=25;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        StartCoroutine(PosicionarEnzima());
+        velocidadRotacion=new Vector3(Random.Range(0, limitMax),Random.Range(0, limitMax), Random.Range(0, limitMax));
+    }
+
+    void Update()
+    {
+        if (particulaHija != null && activateRotation)
+        {
+            particulaHija.Rotate(velocidadRotacion * Time.deltaTime);
+        }
     }
 
     IEnumerator ActivarGravedad()
@@ -24,7 +42,28 @@ public class S_Enzima : MonoBehaviour
         if (collision.gameObject.CompareTag("Manos"))
         {
             StartCoroutine(ActivarGravedad());
+            Vector3 direccion = (transform.position - collision.transform.position).normalized;
+            float fuerzaEmpuje = 0.2f; // Ajusta este valor según el efecto deseado
+
+            rb.AddForce(direccion * fuerzaEmpuje, ForceMode.Impulse);
         }
+
+        if (collision.gameObject.CompareTag("Estomago"))
+        {
+            activateRotation = false;
+            efectoPepsina.SetActive(true);
+        }
+    }
+
+    IEnumerator PosicionarEnzima()
+    {
+        Vector3 posicionFinal=new Vector3(transform.position.x,finalPosY,transform.position.z);
+        while (Mathf.Abs(transform.position.y - finalPosY) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, posicionFinal, velocidad * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+        transform.position = posicionFinal;
     }
 }
 
--- Assets/Resources/Scripts/S_SpawnObject.cs
+++ Assets/Resources/Scripts/S_SpawnObject.cs
@@ -10,6 +10,7 @@ public class S_SpawnObject : MonoBehaviour
 
     public float time;
     public int totalObjSpawn = 10;
+    public List<S_Enzima> enzimas;
     // Start is called before the first frame update  
     void Start()
     {
@@ -28,7 +29,28 @@ public class S_SpawnObject : MonoBehaviour
         {
             Vector3 randomPos = transform.position + Random.insideUnitSphere * radius;
             randomPos.x = transform.position.x + Random.Range(-limitX, limitX);
-            Instantiate(prefab, randomPos, Quaternion.identity);
+            GameObject obj = Instantiate(prefab, randomPos + Vector3.up * 10, Quaternion.identity);
+            obj.GetComponent<S_Enzima>().finalPosY = randomPos.y;
+            enzimas.Add(obj.GetComponent<S_Enzima>());
+        }
+    }
+
+    public void GravedadEnzimas()
+    {
+        foreach (S_Enzima enzima in enzimas)
+        {
+            if (!enzima.GetComponent<Rigidbody>().useGravity)
+            {
+                enzima.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+
+    public void OffEnzimas()
+    {
+        foreach (S_Enzima enzima in enzimas)
+        {
+            enzima.gameObject.SetActive(false);
         }
     }
 }
